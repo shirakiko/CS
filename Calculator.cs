0a8f0268952530897d5fe1f1679a460e0577fbdb
shirakiko <3036797674@qq.com>
2018-12-22 12:52:12 +0800

    try

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+namespace Helloworld
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            double a,b;
+            double d = 0;
+            Console.WriteLine("Please enter a number 'a'.");       
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a  number 'b'. ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a signal.");
+            string c = Console.ReadLine();
+            switch (c)
+            {
+                case "+":
+                    d = a + b;
+                    break;
+
+                case "-":
+                    d = a - b;
+                    break;
+
+
+                case "*":
+                    d = a * b;
+                    break;
+                case "/":
+                    d = a / b;
+                    break;
+
+                default:
+                    break;
+            }
+                  Console.WriteLine(d);     
+        }
+    }
+}
